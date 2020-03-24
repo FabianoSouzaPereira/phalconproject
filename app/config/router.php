5d344c803db72394f6120a312c5d770ace2e3808
Fabiano de Souza Pereira <jeovajire2003@gmail.com>
2020-03-24 11:05:18 -0300

    First commit
    Starting project.

---
+++ app/config/router.php
@@ -0,0 +1,13 @@
+<?php
+
+$router = $di->getRouter();
+
+$router->add(
+    '/app/controller/',
+    [
+        'controller' => 'index',
+        'Action'     => 'index',
+    ]
+    );
+
+$router->handle($_SERVER['REQUEST_URI']);
