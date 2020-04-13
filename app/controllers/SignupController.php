aa1e84c6b146f99aa50d3acde750920e72584a7d
Fabiano de Souza Pereira <jeovajire2003@gmail.com>
2020-04-13 17:25:36 -0300

    Update SignupController.php

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -1,9 +1,12 @@
 <?php
 declare(strict_types=1);
+use Phalcon\Db\Column;
+use Phalcon\Http\Request;
 
 class SignupController extends ControllerBase
 {
 
+    
     public function indexAction()
     {
 
@@ -12,6 +15,9 @@ class SignupController extends ControllerBase
     public function registerAction()
     {
         $user = new Users();
+        $nome=null;
+        $email=null;
+        $senha=null;
         
         //assign value from the form to $user
         $user->assign(
@@ -24,15 +30,41 @@ class SignupController extends ControllerBase
         $this->view->success = $success;
         
         if ($success) {
-        echo    $message = "Obrigado por Registrar";
+        $message = "Obrigado por Registrar";
+
         } else {
-         echo   $message = "Desculpe, ocorreu os seguintes problemas: <br>"
-                . implode('<br>', $user->getMessages());
-        }
+
+            $msg=$user->getMessages();
+            $mString="";
+
+                foreach ($msg as $key => $value){
+                   $i=$key;
+                   $vlr= $key = $value;
+        
+                   if ($vlr->getField() == 'username'){ 
+                       $nome= "<h2>Preencha o campo nome corretamente.<h2/>"; 
+                   };
+                   if ($vlr->getField()  == 'useremail'){ 
+                       $email= "<h2>Preencha o campo email corretamente.<h2/>"; 
+                   };
+                   if ($vlr->getField()  == 'userpassword'){ 
+                       $senha= "<h2>Preencha o campo senha corretamente.<h2/>"; 
+                   };
+              
+                }
+                
+         $message= "<div class='alert02'>"
+                       . "<div class='alert alert-danger fade show margem' role='alert'>"
+                            . "<h2>Desculpe, ocorreu os seguintes problemas:<h2/>"
+                            . $nome . $email .  $senha 
+                       . "</div>"
+                            . "<p>". $this->tag->linkTo(['/signup/', 'voltar', 'class' => 'btn btnvoltar btnstyle']) . "</p>"
+                   . "</div>";
+            }
         
         // passing a message to the view
-        $this->view->message = $message;
- 
+            $this->view->message = print $message;
+        $this->view->disable();
     }
-    
+
 }
