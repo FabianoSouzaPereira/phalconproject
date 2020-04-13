a7f6361b20e1a0b8b35434c57b2c8e77aaec84dd
Fabiano de Souza Pereira <jeovajire2003@gmail.com>
2020-04-13 17:21:26 -0300

    application DIR changed.

--- app/config/config.php
+++ app/config/config.php
@@ -4,11 +4,6 @@
  * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
  * NOTE: please remove this comment.
  */
-        /** Path that comes these variable in project time:
-        *   APP_PATH  = C:\xampp\htdocs\phalconproject/app
-        *   BASE_PATH = C:\xampp\htdocs\phalconproject
-        */ 
-
 defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
 defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
@@ -22,7 +17,7 @@ return new \Phalcon\Config([
         'charset'     => 'utf8',
     ],
     'application' => [
-       // 'appDir'         => APP_PATH . '/',
+        'appDir'         => APP_PATH . '/',
         'controllersDir' => APP_PATH . '/controllers/',
         'modelsDir'      => APP_PATH . '/models/',
         'migrationsDir'  => APP_PATH . '/migrations/',
@@ -30,6 +25,6 @@ return new \Phalcon\Config([
         'pluginsDir'     => APP_PATH . '/plugins/',
         'libraryDir'     => APP_PATH . '/library/',
         'cacheDir'       => BASE_PATH . '/cache/',
-        'baseUri'        => preg_replace('/public([\/\\\\])index.php$/', '', $_SERVER["PHP_SELF"]),
+        'baseUri'        => '/phalconproject/',
     ]
 ]);
